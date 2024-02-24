6da5b6dff117d4d9eea22418e1d78c4dc8ffc4e6
psahithireddy <sahithireddy5422@gmail.com>
2024-02-24 17:01:52 +0530

    Add capa testing

---
+++ tests/data/.github/check_sample_filenames.py
@@ -0,0 +1,100 @@
+# Copyright (C) 2023 Mandiant, Inc. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at: [package root]/LICENSE.txt
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+#  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+"""
+Check testfiles data directory for consistent naming.
+"""
+
+import os
+import sys
+import string
+import hashlib
+import logging
+import os.path
+import argparse
+from pathlib import Path
+
+logger = logging.getLogger("capa.tests.data")
+
+IGNORED_EXTS = (".md", ".txt", ".git", ".gitattributes", ".gitignore", ".gitmodules", ".json")
+VALID_EXTS = (".exe_", ".dll_", ".elf_", ".sys_", ".raw32", ".raw64", ".aspx_", ".cs_", ".py_", ".json.gz")
+IGNORED_DIRS = (".git", ".github", "sigs")
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("testfiles", type=str, help="Path to tests/data")
+    args = parser.parse_args(args=argv)
+
+    test_failed = test_data_filenames(args)
+    if test_failed:
+        return 1
+    else:
+        logger.info("test files look good!")
+        return 0
+
+
+def test_data_filenames(args):
+    test_failed = False
+    for root, _, files in os.walk(args.testfiles):
+        root = Path(root)
+        # Skip ignored directories
+        if any((ignored_dir in root.parts) for ignored_dir in IGNORED_DIRS):
+            continue
+
+        for filename in files:
+            if filename.endswith(IGNORED_EXTS):
+                continue
+
+            path: Path = root / filename
+
+            if not filename.endswith(VALID_EXTS):
+                logger.error("invalid file extension: %s", path)
+                test_failed = True
+                continue
+
+            name = path.stem
+            if all(c in string.hexdigits for c in name):
+                try:
+                    hashes = get_file_hashes(path)
+                except IOError:
+                    continue
+
+                # MD5 file name
+                if len(name) == 32:
+                    if hashes["md5"] != name:
+                        logger.error("invalid file name: %s, MD5 hash: %s", path, hashes["md5"])
+                        test_failed = True
+                # SHA256 file name
+                elif len(name) == 64:
+                    if hashes["sha256"] != name:
+                        logger.error("invalid file name: %s, SHA256 hash: %s", path, hashes["sha256"])
+                        test_failed = True
+                else:
+                    logger.error("invalid file name: %s, should be MD5 or SHA256 hash", path)
+                    test_failed = True
+    return test_failed
+
+
+def get_file_hashes(path: Path):
+    buf = path.read_bytes()
+
+    md5 = hashlib.md5()
+    md5.update(buf)
+
+    sha256 = hashlib.sha256()
+    sha256.update(buf)
+
+    return {"md5": md5.hexdigest().lower(), "sha256": sha256.hexdigest().lower()}
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    sys.exit(main())
---
+++ tests/data/README.md
@@ -0,0 +1,28 @@
+# Resources for testing capa
+Data to test capa's [code](https://github.com/mandiant/capa) and [rules](https://github.com/mandiant/capa-rules).
+
+## Naming conventions
+We use the following conventions to organize the capa test data.
+
+- File name
+  - MD5 or SHA256 hash, all lower case, e.g.
+    - `d41d8cd98f00b204e9800998ecf8427e`
+    - `e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855`
+  - Descriptive name, e.g.
+    - `kernel32`
+    - `Practical Malware Analysis Lab 01-01`
+- File extension
+  - `.exe_`
+  - `.dll_`
+  - `.sys_`
+  - `.elf_`
+  - `.raw32` (32-bit shellcode)
+  - `.raw64` (64-bit shellcode)
+  - `.cs_` (C# source code)
+  - `.aspx_` (ASP.NET source code)
+  - `.py_` (Python source code)
+- Directories
+  - `/`: native test binaries
+  - `/dotnet`: .NET test binaries
+  - `/sigs`: test signatures
+  - `/source`: source language test files e.g. C# and Python
